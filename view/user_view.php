6036af89a1531c59bc88c4e4ad06db50d2bce64f
takahiro <ttakka365@gmail.com>
2020-07-07 21:58:53 +0900

    修正完了

--- view/user_view.php
+++ view/user_view.php
@@ -3,23 +3,33 @@
     <head>
         <title>World Beer Shop｜ユーザー管理</title>
         <?php include VIEW_PATH . 'templates/head.php'; ?>
+        <?php include VIEW_PATH . 'templates/header_bootstrap.php'; ?>
         <link rel="stylesheet" href="../html/assets/css/user.css">
     </head>
     <body>
         <h1>ユーザー管理ページ</h1>
-        <a href="../html/admin.php">商品管理ページはこちら</a>
+        <p><a href="../html/admin.php">商品管理ページはこちら</a></p>
+        <p><a href="../html/customer_history.php">購入者情報ページはこちら</a></p>
         <h2>ユーザー情報</h2>
-        <table>
-        <tr>
-            <th>ユーザー名</th>
-            <th>登録日</th>
-        </tr>
-        <?php foreach($users as $value) { ?>
-        <tr>
-            <td><?php print $value['username'];?></td>
-            <td><?php print $value['createdate'];?></td>
-        </tr>
-        <?php } ?>
-        </table>
+        <div class="container">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ユーザー名</th>
+                            <th>登録日</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($users as $value) { ?>
+                        <tr>
+                            <td><?php print $value['username'];?></td>
+                            <td><?php print $value['createdate'];?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
